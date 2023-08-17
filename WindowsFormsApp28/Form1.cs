f6278f138be535922e83f749f23bbeb1d395ffa9
Waken <votrevincentwaken@gmail.com>
2023-08-17 20:33:36 +0300

    Add project files.

---
+++ WindowsFormsApp28/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApp28.Models;
+
+namespace WindowsFormsApp28
+{
+    public partial class Form1 : Form
+    {
+        static public DataBase Db_ = new DataBase();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = Db_.SearchByName(textBox1.Text);
+            dataGridView1.Columns[0].Width = 50;
+            dataGridView1.Columns[1].Width = 100;
+            dataGridView1.Columns[2].Width = 100;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = Db_.SearchByAuthor(textBox1.Text);
+            dataGridView1.Columns[0].Width = 100;
+            dataGridView1.Columns[1].Width = 150;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = Db_.SearchByCategories(textBox1.Text);
+            dataGridView1.Columns[0].Width = 100;
+            dataGridView1.Columns[1].Width = 150;
+        }
+    }
+}
---
+++ WindowsFormsApp28/Models/DataBase.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data;
+
+namespace WindowsFormsApp28.Models
+{
+    public class DataBase
+    {
+        SqlConnection connection = null;
+        DataSet dataSet = new DataSet();
+        SqlDataAdapter adapter = new SqlDataAdapter();
+        public DataBase()
+        {
+            connection = new SqlConnection();
+            connection.ConnectionString = ConfigurationManager.ConnectionStrings["library"].ConnectionString;
+        }
+
+        public DataTable SearchByName(string name)
+        {
+            dataSet.Tables.Clear();
+
+            var selectCommand = connection.CreateCommand();
+            selectCommand.CommandText = $"SELECT B.Name, A.FirstName+' '+A.LastName as 'Author Name', P.Name " +
+                $"from Books as B join Authors AS A on B.Id_Author=A.Id join Press AS P on B.Id_Press=P.Id WHERE B.Name LIKE '%{name}%'";
+
+            adapter.SelectCommand = selectCommand;
+
+            adapter.Fill(dataSet);
+
+            return dataSet.Tables[0];
+        }
+
+        public DataTable SearchByAuthor(string name)
+        {
+            dataSet.Tables.Clear();
+
+            var selectCommand = connection.CreateCommand();
+            selectCommand.CommandText = $"SELECT B.Name, A.FirstName+' '+A.LastName " +
+                $"FROM Books AS B join Authors AS A on B.Id = A.Id " +
+                $"WHERE A.FirstName LIKE '%{name}%' OR A.LastName LIKE '%{name}%'";
+
+            adapter.SelectCommand = selectCommand;
+
+            adapter.Fill(dataSet);
+
+            return dataSet.Tables[0];
+        }
+
+        public DataTable SearchByCategories(string name)
+        {
+            dataSet.Tables.Clear();
+
+            var selectCommand = connection.CreateCommand();
+            selectCommand.CommandText = $"SELECT B.Name, C.Name FROM Books AS B join Categories AS C on B.Id = C.Id WHERE C.Name LIKE '%{name}%'";
+
+            adapter.SelectCommand = selectCommand;
+
+            adapter.Fill(dataSet);
+
+            return dataSet.Tables[0];
+        }
+    }
+
+
+}
